23ee85f229661898eab7ce03e2489e894f2222d9
Lob26 <69770518+Lob26@users.noreply.github.com>
2020-10-23 21:56:00 -0500

    Entrega final

--- App/model.py
+++ App/model.py
@@ -243,23 +243,21 @@ def getAccidentsByRange(analyzer, initialDate, finalDate):
 
     #Si el la suma de los accidentes da 0 (Si no hay accidentes en el rango de fechas) eso significa que no hay maximo
     if total == 0:
-        k = 'Ninguno'
+        return (total, "Ninguno")
+        
+    mostCommon = counter1
+    k = '1'
+    if counter2 > mostCommon:
+        mostCommon = counter2
+        k = '2'
 
-    #Calcular maximo
-    else:
-        mostCommon = counter1
-        k = '1'
-        if counter2 > mostCommon:
-            mostCommon = counter2
-            k = '2'
+    if counter3 > mostCommon:
+        mostCommon = counter3
+        k = '3'
 
-        if counter3 > mostCommon:
-            mostCommon = counter3
-            k = '3'
-
-        if counter4 > mostCommon:
-            mostCommon = counter4
-            k = '4'
+    if counter4 > mostCommon:
+        mostCommon = counter4
+        k = '4'
 
     return total, k
 
